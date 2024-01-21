b3429400575366e3094bccc3b993d653d29469fe
Oluwadamilola Ajayi <oluwadamilola.william@gmail.com>
2024-01-21 03:27:35 +0100

    creating a time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
-// DOM Elements
-var currentDayEl = $("#currentDay")
-
-
-
-// Display current time function
+// Function to display the current day at the top of the calendar
 function displayTime() {
-    var rightNow = dayjs().format('DD MMM YYYY [at] hh:mm:ss a');
-    currentDayEl.text(rightNow);
+    var rightNow = dayjs().format("dddd, MMMM D, YYYY");
+    $("#currentDay").text(rightNow);
   }
+  
+  // Function to create timeblocks for standard business hours
+  function createTimeblocks() {
+    var container = $(".container");
+  
+    // Loop through hours (assuming 9 AM to 5 PM)
+    for (var hour = 9; hour <= 17; hour++) {
+      // Create elements for timeblock
+      var timeblock = $("<div>").addClass("row time-block");
+      var hourColumn = $("<div>").addClass("col-md-1 hour").text(hour + ":00 AM");
+      var eventColumn = $("<textarea>").addClass("col-md-10 description").css("color", "black");
+  
+    }
+  }
+  
 
-
-
-
-
-
-
-
-
-
-
-
+  
+  // Call functions to initialize the app
   setInterval(displayTime, 1000);
-
-
+  createTimeblocks();
+ 
+  
